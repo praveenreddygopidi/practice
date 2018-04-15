534f7e514a9a8e8cf2a3cf6d4326e93e7559393d
praveenreddygopidi@gmail.com <praveenreddygopidi@gmail.com>
2018-04-15 15:59:01 +0530

    Build Json Object

--- src/main/java/JsonProcessing/ProcessJSON1.java
+++ src/main/java/JsonProcessing/ProcessJSON1.java
@@ -10,7 +10,20 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class ProcessJSON1 {
-    public static String jsonObjectFile ="E:\\Training\\JsonProcessing\\src\\main\\java\\JsonProcessing\\jsonData.json\\";
+    //public static String jsonObjectFile ="E:\\Training\\JsonProcessing\\src\\main\\java\\JsonProcessing\\jsonData.json\\";
+    public static String jsonObjectFile ="src/main/java/JsonProcessing/jsonData.json";
+
+    public static void main(String args[]) throws Exception{
+        ProcessJSON1 processJSON1 = new ProcessJSON1();
+        //ONE
+        /*ProcessJSON1 processJSON1 = new ProcessJSON1();
+        JsonObject object = processJSON1.loadJsonData();
+        System.out.println(object);*/
+
+        //TWO
+        JsonObject jsonObjectBuilder = processJSON1.buildJsonObject();
+        System.out.println(jsonObjectBuilder);
+    }
 
     public JsonObject loadJsonData () throws FileNotFoundException {
         JsonReader jsonReader = Json.createReader(new FileReader(jsonObjectFile));
@@ -18,11 +31,16 @@ public class ProcessJSON1 {
         return jsonObject;
     }
 
-    public static void main(String args[]) throws Exception{
-        ProcessJSON1 processJSON1 = new ProcessJSON1();
-
-        JsonObject object = processJSON1.loadJsonData();
-
-        System.out.println(object);
+    public JsonObject buildJsonObject(){
+        JsonObject jsonObject = Json.createObjectBuilder()
+                .add("name", "praveen")
+                .add("profession", "Software Developer")
+                .add("hobbies", Json.createArrayBuilder().add("cricket")
+                                                            .add("programmer")
+                                                            .add("traveller").build())
+                .add("place", "noida")
+                .add("age", 25)
+                .build();
+        return jsonObject;
     }
 }
